f4158e0babb9520261e71c477f85a6e538550caf
Unknown <skyesladeut@outlook.com>
2018-01-18 11:16:07 -0700

    Added a cube and script which prints text

---
+++ FirstGame/Assets/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	//this is declaring what start will do
+	void Start () {
+		print("My animal is eating.");
+		print("Now my animal is sleeping.");
+		print("I love my cat.");
+	}
+	
+	void OnMouseDown()
+	{
+		print("Meow.");
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- FirstGame/Assets/SecondScript.cs
+++ FirstGame/Assets/SecondScript.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Class is an object classification
+//You must have open and closed {} per object
 public class SecondScript : MonoBehaviour {
 
+//void is a function that is void or empty or null
 	// Use this for initialization
 	void Start () {
 		
