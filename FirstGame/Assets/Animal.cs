e0a27736f6c406e97e6843fb0582bef993647ebb
Unknown <skyesladeut@outlook.com>
2018-01-25 18:47:33 -0700

    Added 5 variables into Animal Script with comments

--- FirstGame/Assets/Animal.cs
+++ FirstGame/Assets/Animal.cs
@@ -5,25 +5,33 @@ using UnityEngine.UI;
 
 
 public class Animal : MonoBehaviour {
-
+//string variables are for assigning strings of letters (uses " ")
 	public string Name;
+//int variables are for whole numbers
 	public int FoodCount;
+	public int TimesClicked;
+//text variables are for displaying UI text on the screen of the game
 	public Text myUIText;
+//float variables are numbers which can have decimal places
+	public float randomNum;
 	
 	void OnMouseDown()
 	{
 		if (FoodCount >= 0) {
 			myUIText.text = "The " + Name + " ate food. \nThe " + Name + " has " + FoodCount + " amount of food left.";
 			FoodCount -= 1;
+			TimesClicked += 1;
 		}
 
 		if ((FoodCount < 0) && (FoodCount >=-5)) {
-			print("The " + Name + " is starving.");
+			myUIText.text = "The " + Name + " is starving.";
 			FoodCount -= 1;
+			TimesClicked += 1;
 		}
 
 		if (FoodCount < -5) {
-			print("The " + Name + " is dead.");
+			myUIText.text = "The " + Name + " is dead.";
+			TimesClicked += 1;
 		}
 
 	}
