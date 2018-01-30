64371721bcfbdad9355efa98a3f55c28374a3084
Unknown <skyesladeut@outlook.com>
2018-01-30 11:16:18 -0700

    Added if statements and bools to KeyClass

--- FirstGame/Assets/Animal.cs
+++ FirstGame/Assets/Animal.cs
@@ -9,29 +9,25 @@ public class Animal : MonoBehaviour {
 	public string Name;
 //int variables are for whole numbers
 	public int FoodCount;
-	public int TimesClicked;
 //text variables are for displaying UI text on the screen of the game
 	public Text myUIText;
 //float variables are numbers which can have decimal places
-	public float randomNum;
 	
 	void OnMouseDown()
 	{
+
 		if (FoodCount >= 0) {
 			myUIText.text = "The " + Name + " ate food. \nThe " + Name + " has " + FoodCount + " amount of food left.";
-			FoodCount -= 1;
-			TimesClicked += 1;
+			FoodCount -= 1;	
 		}
 
 		if ((FoodCount < 0) && (FoodCount >=-5)) {
 			myUIText.text = "The " + Name + " is starving.";
 			FoodCount -= 1;
-			TimesClicked += 1;
 		}
 
 		if (FoodCount < -5) {
 			myUIText.text = "The " + Name + " is dead.";
-			TimesClicked += 1;
 		}
 
 	}
---
+++ FirstGame/Assets/KeyClass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyClass : MonoBehaviour {
+
+	public bool Key = false;
+	public bool Ax = false;
+	public Text myUIText;
+	void OnMouseDown()
+	{
+		if(Key == true) {
+			myUIText.text = "Open Door";
+		}
+		if((Key == false) && (Ax == true)) {
+			myUIText.text = "Chop down the door";
+		}
+		if((Key == false) && (Ax == false)) {
+			myUIText.text = "Door cannot be opened";
+		}
+	}
+
+
+}
