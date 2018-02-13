ed4d7bd465551f9d725edba2e7d8eafb6ca1c934
Unknown <skyesladeut@outlook.com>
2018-02-13 15:05:02 -0700

    foreach loops and array comments and examples

--- FirstGame/Assets/Scripts/ArraysAndLoops.cs
+++ FirstGame/Assets/Scripts/ArraysAndLoops.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class ArraysAndLoops : MonoBehaviour {
 
     //arrays are objects which contain lists of elements
-    //[] make the variable into an array
+    //[] makes the variable into an array
+    //arrays can have many different types such as double, int, string, scriptable objects, etc.
     public string[] playerNames;
     public int[] HighScores;
     public PowerUp[] PowerUps;
@@ -14,24 +15,29 @@ public class ArraysAndLoops : MonoBehaviour {
     public string[] Passwords;
     public int[] RandomNums;
     public Player[] Players;
+    public double[] RandomDecimals;
+    public string[] keyWords;
 
     void Start () {
         //loop which goes through each element in an array
         //foreach(datatype variable in Array)
         //assigns the variable with each element in the array
+        //the loop ends once it has iterates through all elements in an array
         foreach (var player in playerNames) {
-            //prints each players' names
+            //prints each of the player's names
             print (player);
         }
         //to access a single element in an array use arrayName[index]
         //index numbers start at 0
         //ex: Player[0] would give the first player
         foreach (var score in HighScores) {
+            //goes through all the numbers in HighScores and prints if it is greater than 1000
             if (score >= 1000) {
                 print (score);
             }
         }
         foreach(var player in Players) {
+            //for every Player scriptable objects, it prints the name, score and MP
             print("Player: " + player.PlayerName);
             print("Score: " + player.Score);
             print("MPs " + player.MPs);
