9b22e05fc9fc37087d119f921d3a253b6f8bb47d
Unknown <skyesladeut@outlook.com>
2018-03-01 11:17:47 -0700

    Functions with parameters
    and finished the player controller

---
+++ FirstGame/Assets/Scripts/FuntionsWithParameters.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuntionsWithParameters : MonoBehaviour {
+
+	public int Health = 20;
+	//functions with parameters
+	//a function can take an object inside the parantheses
+	//Collider is an identifier and other is the object name
+	void OnTriggerEnter(Collider other)
+	{
+		//print("Hit Something");
+		//print(other);
+
+		//AddNumbers(14, 10);
+		//PlayerData("George");
+		AddHealth(25);
+	}
+
+	void AddNumbers(int a, int b) {
+		print(a += b);
+	}
+
+	void PlayerData(string name) {
+		print("This player is " + name);
+	}
+
+	void AddHealth(int newHealth) {
+		Health += newHealth;
+	}
+}
--- FirstGame/Assets/Scripts/PlayerController.cs
+++ FirstGame/Assets/Scripts/PlayerController.cs
@@ -4,51 +4,57 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	Rigidbody RB;
-	public float jumpSpeed;
-	public float sideSpeed;
-	public int PlayerNum;
-	private bool isGrounded;
-	private int jumpCount;
-	//public GameObject myObject;
+    Rigidbody RB;
+    public float jumpSpeed;
+    public float sideSpeed;
+    public int PlayerNum;
+    private bool isGrounded;
+    private int jumpCount;
 
-	void Start () {
-		RB = GetComponent<Rigidbody>();
-		isGrounded = true;
-		jumpCount = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-		Jump();
-		Move();
-	}
-	void Jump() {
-		if(isGrounded || jumpCount < 2){
-			if(Input.GetButtonDown("Vertical" + PlayerNum)) {
-				if (Input.GetAxisRaw("Vertical" + PlayerNum) > 0) {
-					RB.velocity = Vector3.up * jumpSpeed;
-					isGrounded = false;
-					++jumpCount;
-				}
-			}
-		}
-	}
+    void Start () {
+        RB = GetComponent<Rigidbody> ();
+        isGrounded = true;
+        jumpCount = 0;
+    }
 
-	void OnCollisionEnter(Collision other)
-	{
-		isGrounded = true;
-		jumpCount = 0;
-	}
-	void Move() {
-		if(Input.GetButton("Horizontal" + PlayerNum)) {
-			if (Input.GetAxisRaw("Horizontal" + PlayerNum) > 0) {
-				RB.velocity = Vector3.right * sideSpeed;
-			}
-			else {
-				RB.velocity = Vector3.left * sideSpeed;
-			}
-		}
-	}
+    void Update () {
+
+        Jump ();
+        Move ();
+    }
+    void Jump () {
+        if (isGrounded || jumpCount < 2) {
+            if (Input.GetButtonDown ("Vertical" + PlayerNum)) {
+                if (Input.GetAxisRaw ("Vertical" + PlayerNum) > 0) {
+                    RB.velocity = Vector3.up * jumpSpeed;
+                    isGrounded = false;
+                    ++jumpCount;
+                }
+            }
+        }
+    }
+
+    void OnCollisionEnter (Collision other) {
+        isGrounded = true;
+        jumpCount = 0;
+    }
+    void Move () {
+        if (isGrounded) {
+            if (Input.GetButton ("Horizontal" + PlayerNum)) {
+                if (Input.GetAxisRaw ("Horizontal" + PlayerNum) > 0) {
+                    RB.velocity = Vector3.right * sideSpeed;
+                } else {
+                    RB.velocity = Vector3.left * sideSpeed;
+                }
+            }
+        } else {
+            if (Input.GetButtonDown ("Horizontal" + PlayerNum)) {
+                if (Input.GetAxisRaw ("Horizontal" + PlayerNum) > 0) {
+                    RB.velocity = Vector3.right * sideSpeed;
+                } else {
+                    RB.velocity = Vector3.left * sideSpeed;
+                }
+            }
+        }
+    }
 }
