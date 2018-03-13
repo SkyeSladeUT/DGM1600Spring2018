d1307a735137421ff6115c604f61cb3c8d4302ce
Unknown <skyesladeut@outlook.com>
2018-03-13 15:21:36 -0600

    added more examples of functions in FunctionsWithReturns script

--- FirstGame/Assets/Scripts/Functions/FunctionsWithReturns.cs
+++ FirstGame/Assets/Scripts/Functions/FunctionsWithReturns.cs
@@ -43,4 +43,39 @@ public class FunctionsWithReturns : MonoBehaviour {
 		return name;
 	}
 
+	public int AddNum(int a, int b) {
+		return a + b;
+	}
+
+	public float Divide(float a, float b) {
+		return a / b;
+	}
+
+	public int SubNum(int a, int b) {
+		return a - b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public float QuadraticEquationPositive (float a, float b, float c) {
+		float x = -b + Mathf.Sqrt((b*b) - (4 * a * c));
+		x = x/(2*a);
+		return x;
+	}
+
+	public string MadLibs(string noun, string adjective, string verb) {
+		string phrase;
+		phrase = "The " + adjective + " " + noun + " is " + verb + ".";
+		return phrase;
+	}
+
+	public int getNewNumber(int a) {
+		a += 12;
+		a *= 2;
+		a -= 10;
+		return a;
+	}
+
 }
