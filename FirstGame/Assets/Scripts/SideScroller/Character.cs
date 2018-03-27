7a56c10b76c9a748a21d282206e485662e9f4313
Unknown <skyesladeut@outlook.com>
2018-03-27 14:23:55 -0600

    Character Controller

---
+++ FirstGame/Assets/Scripts/SideScroller/Character.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Character : ScriptableObject {
+
+	public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    public Vector3 moveDirection = Vector3.zero;
+
+		
+}
---
+++ FirstGame/Assets/Scripts/SideScroller/MoveCharacterTwo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacterTwo : MonoBehaviour {
+
+	public Character myCharacter;
+    private CharacterController controller;
+
+	private void Start() {
+		controller = GetComponent<CharacterController>();
+	}
+
+    void Update() {
+        if (controller.isGrounded) {
+
+			myCharacter.moveDirection.x = Input.GetAxis("Horizontal");
+			myCharacter.moveDirection.z = Input.GetAxis("Vertical");
+			myCharacter.moveDirection.y = 0;
+
+            myCharacter.moveDirection = transform.TransformDirection(myCharacter.moveDirection);
+            myCharacter.moveDirection *= myCharacter.speed;
+            if (Input.GetButton("Jump"))
+                myCharacter.moveDirection.y = myCharacter.jumpSpeed;
+        }
+		//Time.deltaTime makes it run in real time rather than every frame
+        myCharacter.moveDirection.y -= myCharacter.gravity * Time.deltaTime;
+        controller.Move(myCharacter.moveDirection * Time.deltaTime);
+    }	
+}
