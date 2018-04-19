30899cb0b3ab06235fc21e74bbebc900ccf67cb3
Unknown <skyesladeut@outlook.com>
2018-04-18 19:55:46 -0600

    Work on Doors and Buttons and finished the flooring

--- SideScroller/Assets/Scripts/Key.cs
+++ SideScroller/Assets/Scripts/Key.cs
@@ -6,32 +6,40 @@ public class Key : MonoBehaviour {
 
 	public GameObject[] Door1;
 	public GameObject[] Door2;
-	private bool CurrentButtonActive = true;
 	private Animation animate;
+	public ButtonScript button;
+	private bool isButtonActive;
+
 
 	private void Start () {
 		animate = gameObject.GetComponent<Animation> ();
 		foreach (var door in Door2) {
 			door.SetActive (false);
 		}
-		CurrentButtonActive = true;
 
 	}
-	private void OnTriggerExit (Collider other) {
-
-		if (CurrentButtonActive) {
-			animate.Play ("On");
-		} else {
-			animate.Play ("Off");
-		}
+	private void OnTriggerEnter (Collider other) {
+		if(isButtonActive)
+			animate.Play("On");
+		else
+			animate.Play("Off");
 		foreach (var door in Door1) {
-			door.SetActive (!CurrentButtonActive);
+			door.SetActive (button.CurrentButtonActive);
 		}
 		foreach (var door in Door2) {
-			door.SetActive (CurrentButtonActive);
+			door.SetActive (!button.CurrentButtonActive);
 		}
-
-		CurrentButtonActive = !CurrentButtonActive;
+		button.CurrentButtonActive = !button.CurrentButtonActive;
+		isButtonActive = !isButtonActive;
 	}
-
+	/*private void OnTriggerStay(Collider other)
+	{
+		foreach (var door in Door1) {
+			door.SetActive (!button.CurrentButtonActive);
+		}
+		foreach (var door in Door2) {
+			door.SetActive (button.CurrentButtonActive);
+		}
+		button.CurrentButtonActive = !button.CurrentButtonActive;
+	}*/
 }
